4fdd30924295ea1d17681746cac5d7aafa66c5c5
Arun-Jawlia <101392646+Arun-Jawlia@users.noreply.github.com>
2023-01-28 16:30:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.set('strictQuery':false)
+mongoose.set('strictQuery',false)
 
 const connection = mongoose.connect(process.env.mongoURL)
 // const connection = mongoose.connect('mongodb://127.0.0.1:27017/masaiquiz')
